17b8839b11f2fcd191c836c75b9927f3977d29ec
fyodor <13544102930@163.com>
2020-02-24 14:12:03 +0800

    update

--- components/layouts/header.vue
+++ components/layouts/header.vue
@@ -2,7 +2,7 @@
     <div class="header">
         <!-- <div class="head-word">Alan Tree</div> -->
         <div class="header-content">
-            <nuxt-link class="head-word" exact to="/">Alan Tree</nuxt-link> 
+            <nuxt-link class="head-word" exact to="/">Fyodor Tree</nuxt-link> 
         </div>
         <!-- <div class="head-word2">All the way forward</div> -->
     </div>
